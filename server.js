5afc6136e59d3dcdfdd3b117de5c18a6dc004658
ana-co <adavi18@freeuni.edu.ge>
2022-08-26 02:40:19 +0400

    upload and stream requested files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import { StatusCodes } from 'http-status-codes';
+import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
 
 import { fileURLToPath } from 'url';
@@ -10,39 +11,73 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+import { createReadStream } from 'streamifier';
 import fs from 'fs';
-import mongodb from 'mongodb';
+
+import mongodb, { ObjectId } from 'mongodb';
 
 import connectMongoDB from './db/mongo_connect.js';
 import connectPostgresDB from './db/pg_connect.js';
+
+import fileUpload from 'express-fileupload';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(fileUpload());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   //   res.json({ msg: 'NODE WELCOME!' });
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get('/upload-test-video', (req, res) => {
+app.post('/upload-media', (req, res) => {
+  const mediaFile = req.files.mediaFile;
+  const filename = mediaFile.name.split('.').slice(0, -1).join('.');
+  // const fileId = uuidv4();
   mongodb.MongoClient.connect(process.env.MONGO_URL, function (error, client) {
     if (error) {
-      res.json(error);
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
       return;
     }
     const db = client.db('media');
     const gfsBucket = new mongodb.GridFSBucket(db);
-    const uploadStream = gfsBucket.openUploadStream('test_video');
-    const readStream = fs.createReadStream('./test_video.mp4');
+    const readStream = createReadStream(mediaFile.data);
+    const uploadStream = gfsBucket.openUploadStream(filename);
+    // ({filename: filename,
+    // fileId:fileId
+    // content_type: mediaFile.mimetype,
+    // metadata: {
+    //     user: req.user
+    // } });
     readStream.pipe(uploadStream);
-    res.status(StatusCodes.OK).send('Media Uploaded...');
+
+    // res.status(StatusCodes.OK).send(fileId);
+    res.status(StatusCodes.OK).send({ mediaFileId: uploadStream.id });
   });
 });
 
-app.get('/stream-video-mongodb', async (req, res) => {
+// app.get('/upload-test-video', (req, res) => {
+//   mongodb.MongoClient.connect(process.env.MONGO_URL, function (error, client) {
+//     if (error) {
+//       res.json(error);
+//       return;
+//     }
+//     const db = client.db('media');
+//     const gfsBucket = new mongodb.GridFSBucket(db);
+//     const uploadStream = gfsBucket.openUploadStream('test_video');
+//     const readStream = fs.createReadStream('./test_video.mp4');
+//     readStream.pipe(uploadStream);
+//     res.status(StatusCodes.OK).send('Media Uploaded...');
+//   });
+// });
+
+app.get('/stream-media', async (req, res) => {
+  const mediaFileId = req.query.mediaFileId;
+  // const mediaFileId = '6307f9c1a5f4f286184e5801';
   mongodb.MongoClient.connect(process.env.MONGO_URL, async (error, client) => {
     if (error) {
       console.log(error);
@@ -57,32 +92,33 @@ app.get('/stream-video-mongodb', async (req, res) => {
 
     const db = client.db('media');
 
-    const video = await db
-      .collection('fs.files')
-      .findOne({ filename: 'test_video' });
-    if (!video) {
+    const mediaFile = await db.collection('fs.files').findOne(
+      // { filename: filename }
+      { _id: ObjectId(mediaFileId) }
+    );
+    if (!mediaFile) {
+      console.log('Media not found');
       return res.status(StatusCodes.NOT_FOUND).send('Media Not Found!');
     }
 
-    const videoSize = video.length;
-    const CHUNK_SIZE = video.chunkSize;
+    const mediaFileSize = mediaFile.length;
+    const CHUNK_SIZE = mediaFile.chunkSize;
+    const mediaFileName = mediaFile.filename;
 
     const start = Number(range.replace(/\D/g, ''));
-    const end = Math.min(start + CHUNK_SIZE, videoSize);
+    const end = Math.min(start + CHUNK_SIZE, mediaFileSize);
     const contentLength = end - start;
-    console.log(`start: ${start}, end: ${end}, range: ${range}`);
     const headers = {
-      'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+      'Content-Range': `bytes ${start}-${end}/${mediaFileSize}`,
       'Accept-Ranges': 'bytes',
       'Content-Length': contentLength,
       'Content-Type': 'video/mp4',
     };
 
-    console.log(headers);
     res.writeHead(StatusCodes.PARTIAL_CONTENT, headers);
 
     const bucket = new mongodb.GridFSBucket(db);
-    const downloadStream = bucket.openDownloadStreamByName('test_video', {
+    const downloadStream = bucket.openDownloadStreamByName(mediaFileName, {
       start,
       end,
     });
@@ -91,22 +127,22 @@ app.get('/stream-video-mongodb', async (req, res) => {
   });
 });
 
-// app.get('/stream-video-local', function (req, res) {
+// app.get('/stream-media-local', function (req, res) {
 //   const range = req.headers.range;
 //   if (!range) {
 //     res.status(400).send('Requires Range header');
 //   }
 
 //   const videoPath = 'test_video.mp4';
-//   const videoSize = fs.statSync('test_video.mp4').size;
+//   const mediaFileSize = fs.statSync('test_video.mp4').size;
 
 //   const CHUNK_SIZE = 10 ** 6; // 1MB
 //   const start = Number(range.replace(/\D/g, ''));
-//   const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+//   const end = Math.min(start + CHUNK_SIZE, mediaFileSize - 1);
 
 //   const contentLength = end - start + 1;
 //   const headers = {
-//     'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+//     'Content-Range': `bytes ${start}-${end}/${mediaFileSize}`,
 //     'Accept-Ranges': 'bytes',
 //     'Content-Length': contentLength,
 //     'Content-Type': 'video/mp4',
