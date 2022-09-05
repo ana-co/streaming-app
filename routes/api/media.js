7a57332b8c4fdfcf370b086a8ab1488fb96cf13c
ana-co <adavi18@freeuni.edu.ge>
2022-09-05 20:40:55 +0400

    streaming unauthorized

--- routes/api/media.js
+++ routes/api/media.js
@@ -11,64 +11,70 @@ const router = express.Router();
 // @route  GET /api/media
 // @desc   Stream media file by media file id
 // access  Private
-router.get('/', authenticateUser, async (req, res) => {
-  const mediaFileId = req.query.mediaFileId;
-  // const mediaFileId = '6309066707c50f17af83887f';
-  try {
-    mongodb.MongoClient.connect(
-      process.env.MONGO_URL,
-      async (error, client) => {
-        if (error) {
-          console.log(error);
-          res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
-          return;
+router.get(
+  '/',
+  /*authenticateUser,*/ async (req, res) => {
+    const mediaFileId = req.query.mediaFileId;
+    // const mediaFileId = '6309066707c50f17af83887f';
+    try {
+      mongodb.MongoClient.connect(
+        process.env.MONGO_URL,
+        async (error, client) => {
+          if (error) {
+            console.log(error);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+            return;
+          }
+
+          const range = req.headers.range;
+          if (!range) {
+            res.status(StatusCodes.BAD_REQUEST).send('Range header required');
+          }
+
+          const db = client.db('media');
+
+          const mediaFile = await db.collection('fs.files').findOne(
+            // { filename: filename }
+            { _id: ObjectId(mediaFileId) }
+          );
+          if (!mediaFile) {
+            console.log('Media not found');
+            return res.status(StatusCodes.NOT_FOUND).send('Media Not Found!');
+          }
+
+          const mediaFileSize = mediaFile.length;
+          const CHUNK_SIZE = mediaFile.chunkSize;
+          const mediaFileName = mediaFile.filename;
+
+          const start = Number(range.replace(/\D/g, ''));
+          const end = Math.min(start + CHUNK_SIZE, mediaFileSize);
+          const contentLength = end - start;
+          const headers = {
+            'Content-Range': `bytes ${start}-${end}/${mediaFileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': contentLength,
+            'Content-Type': 'video/mp4',
+          };
+
+          res.writeHead(StatusCodes.PARTIAL_CONTENT, headers);
+
+          const bucket = new mongodb.GridFSBucket(db);
+          const downloadStream = bucket.openDownloadStreamByName(
+            mediaFileName,
+            {
+              start,
+              end,
+            }
+          );
+
+          downloadStream.pipe(res);
         }
-
-        const range = req.headers.range;
-        if (!range) {
-          res.status(StatusCodes.BAD_REQUEST).send('Range header required');
-        }
-
-        const db = client.db('media');
-
-        const mediaFile = await db.collection('fs.files').findOne(
-          // { filename: filename }
-          { _id: ObjectId(mediaFileId) }
-        );
-        if (!mediaFile) {
-          console.log('Media not found');
-          return res.status(StatusCodes.NOT_FOUND).send('Media Not Found!');
-        }
-
-        const mediaFileSize = mediaFile.length;
-        const CHUNK_SIZE = mediaFile.chunkSize;
-        const mediaFileName = mediaFile.filename;
-
-        const start = Number(range.replace(/\D/g, ''));
-        const end = Math.min(start + CHUNK_SIZE, mediaFileSize);
-        const contentLength = end - start;
-        const headers = {
-          'Content-Range': `bytes ${start}-${end}/${mediaFileSize}`,
-          'Accept-Ranges': 'bytes',
-          'Content-Length': contentLength,
-          'Content-Type': 'video/mp4',
-        };
-
-        res.writeHead(StatusCodes.PARTIAL_CONTENT, headers);
-
-        const bucket = new mongodb.GridFSBucket(db);
-        const downloadStream = bucket.openDownloadStreamByName(mediaFileName, {
-          start,
-          end,
-        });
-
-        downloadStream.pipe(res);
-      }
-    );
-  } catch (err) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err);
+      );
+    } catch (err) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err);
+    }
   }
-});
+);
 
 // @route    POST api/media
 // @desc     Upload media file
